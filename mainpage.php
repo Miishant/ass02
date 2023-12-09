62044b9b2d674f454fdb3713e89364fac5a9d1d5
Miishant <137567648+Miishant@users.noreply.github.com>
2023-12-08 23:08:09 -0500

    Add files via upload

---
+++ mainpage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@300&family=Open+Sans&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <h2>
+            Welcome!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+        </h2>
+    </body>
+    </html>
